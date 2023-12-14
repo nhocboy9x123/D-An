335c129b5fdfd1290a60181aa322a868dcaede04
Dashboard <luanbu4005@gmail.com>
2023-12-14 11:02:40 +0700

    123

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,4 +1,6 @@
 @extends('layout')
 @section('content')
-    <h1>Home</h1>
+    <h1>
+        Home
+    </h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/home',function(){
     return view('Home');
 });
+
+Route::get('/',function(){
+    return view('customer');
+});
